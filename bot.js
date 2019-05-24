dca0a985ee35f5b13e55076d92824f8408b5b9cd
joepietrzyk <joepietrzyk@gmail.com>
2019-05-24 17:34:17 -0400

    made bot ping users if initials are found

--- bot.js
+++ bot.js
@@ -5,32 +5,57 @@ const Discord = require("discord.js");
 
 const bot = new Discord.Client();
 
+var initialWhitelist = require('./whitelist.json');
+const pingChannelID = "473919224218124309";
+const pingMessage = "<@&474053859661185034> <@&553778764132253697> I see whitelisted initials!"
+var targetChannel;
+
 bot.on("ready", async () => {
 
   console.log(`bot is ready!  ${bot.user.tag}`);
-  var targetChannel = bot.channels.get('473919224218124309');
+  targetChannel = bot.channels.get('473919224218124309');
   targetChannel.send("I'm connected.")
 });
 
-
-
 bot.on('message', (message) => {
+  // add to whitelist
+  if (message.channel.id == '580049859445522443') {
+    let newInitials = message.content.match(/-.*$/)[0].replace(/-(.*)$/, "$1")
+      .replace(/ /, '').replace(/ \./, '').toUpperCase();
+    initialWhitelist.whitelist.push(newInitials);
+    fs.writeFile('./whitelist.json', JSON.stringify(initialWhitelist), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+
   // ignores a message if they're not on the bot whitelist
   if (!botSettings.botWhitelist[message.author.id]) { return; }
+
+  if (message.guild.id != '473890977858191380') { return; }
 
   //  let messageEmbed = message.embeds[0]
   //  console.log(messageEmbed)
 
   if (message.embeds.length > 0) {
     if (message.embeds[0].image) {
-      if (message.embeds[0].description.includes('A waifu/husbando appeared') ||
-        message.embeds[0].title.includes('A wild pok')) {
+      if (message.embeds[0].description.includes('A waifu/husbando appeared')) {  //||
+        //message.embeds[0].title.includes('A wild pok')) {
         let proxyURL = message.embeds[0].image.proxyURL;
         console.log(proxyURL);
+        
+        let initialsRegEx = /initials are \'(.*)\'/g;
+        let initials = message.embeds[0].description.match(initialsRegEx)[0].replace(initialsRegEx, '$1')
+          .replace(/\./g, '').replace(/ /g, '').toUpperCase();
+        console.log(initials);
 
+        if (initialWhitelist.whitelist.includes(initials)) {
+          targetChannel.send(pingMessage);
+        }
         //  if(proxyURL.includes('teapot')){ 
-        var ws = fs.createWriteStream('./images/teapot.jpg');
-        request.get(proxyURL).pipe(ws);
+        //var ws = fs.createWriteStream('./images/teapot.jpg');
+        //request.get(proxyURL).pipe(ws);
 
         //  }
 
